c3c6cb29cf8e30916413f3b2bd5d0d69d8bd12db
vazquezlopez <88861677+vazquezlopez@users.noreply.github.com>
2024-09-18 09:59:43 -0600

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$( (cd "$(dirname "$0")" && pwd ))
 echo "[INFO] CICD Pipeline for Apigee X - TeamCity"
 BRANCH_NAME_X=main
 
-SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_DEPLOYMENT_ORG=$APIGEE_ORG"
+SUBSTITUTIONS_X="_DEPLOYMENT_ORG=$APIGEE_ORG"
 SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_APIGEE_TEST_ENV=$APIGEE_ENV"
 SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_API_VERSION=google"
 SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_WORK_DIR=."
@@ -16,6 +16,16 @@ echo "export APIGEE_BUILD_TOKEN=\"$(gcloud auth application-default print-access
 cat env.txt
 echo "[BUILD CONFIG] - Token generado"
 echo "$APIGEE_BUILD_TOKEN"
+sed "s/APIGEE_BUILD_TOKEN=.*/APIGEE_BUILD_TOKEN=[hidden]/g"
+
+source env.txt && \
+mvn clean install -ntp \
+          -P"googleapi" \
+          -Denv="$APIGEE_ENV" \
+          -Dtoken="$${APIGEE_BUILD_TOKEN}" \
+          -Dorg="$_DEPLOYMENT_ORG" \
+          -Ddeployment.suffix="-TeamCity" \
+          -Ddeployment.description="CloudRun Build: $BUILD_ID"
 
 gcloud builds submit --config="$SCRIPTPATH/ci-config/cloudbuild/cloudbuild.yaml" \
   --substitutions="$SUBSTITUTIONS_X"
