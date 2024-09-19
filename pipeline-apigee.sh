4910509ef0a023c669000c52087c5a9667d89b1e
vazquezlopez <88861677+vazquezlopez@users.noreply.github.com>
2024-09-19 10:00:48 -0600

    Update pipeline-apigee.sh

--- pipeline-apigee.sh
+++ pipeline-apigee.sh
@@ -1,16 +1,8 @@
 set -e
 SCRIPTPATH=$( (cd "$(dirname "$0")" && pwd ))
 
-echo "[INFO] CICD Pipeline for Apigee X - TeamCity"
-BRANCH_NAME_X=main
+echo "[INFO] CICD Pipeline for Apigee X - TeamCity - Maven"
 
-SUBSTITUTIONS_X="_DEPLOYMENT_ORG=$APIGEE_ORG"
-SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_APIGEE_TEST_ENV=$APIGEE_ENV"
-SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_API_VERSION=google"
-SUBSTITUTIONS_X="$SUBSTITUTIONS_X,_WORK_DIR=."
-SUBSTITUTIONS_X="$SUBSTITUTIONS_X,BRANCH_NAME=$BRANCH_NAME_X"
-
-echo $SUBSTITUTIONS_X
 
 export TOKEN=$(gcloud auth print-access-token)
 echo "Token was generated"
@@ -22,7 +14,4 @@ mvn clean install -ntp \
           -Dtoken="$TOKEN" \
           -Dorg="$APIGEE_ORG" \
           -Ddeployment.suffix="-TeamCity" \
-          -Ddeployment.description="CloudRun Build: $BUILD_ID"
-
-gcloud builds submit --config="$SCRIPTPATH/ci-config/cloudbuild/cloudbuild.yaml" \
-  --substitutions="$SUBSTITUTIONS_X"
+          -Ddeployment.description="GitHub-TeamCity"
